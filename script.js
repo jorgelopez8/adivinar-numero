b693b43bdaea144b981985f12ee6f81104e4f7d9
jorgelopez8 <123724150+jorgelopez8@users.noreply.github.com>
2023-05-09 12:28:51 +0200

    script

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const numeroAdivinar = Math.floor(Math.random() * 15) + 1;
 
+let intentos = 0;
+
+function pedirNumero() {
+  const numero = prompt('Introduce un número entre 1 y 15, tienes tres intentos');
+  return parseInt(numero);
+}
+
+function comprobarNumero(numero) {
+  intentos++;
+
+  if (numero === numeroAdivinar) {
+    alert(`¡Felicidades! Has acertado el número en ${intentos} intentos.`);
+    return true;
+  } else if (numero < numeroAdivinar) {
+    alert('El número que has introducido es menor.');
+  } else {
+    alert('El número que has introducido es mayor.');
+  }
+  if (intentos === 3 && numero != numeroAdivinar) {
+    alert(`Has agotado tus intentos, el numero era ${numeroAdivinar}.`);
+  }
+
+  return false;
+}
+
+for (let i = 0; i < 3; i++) {
+  const numero = pedirNumero();
+
+  if (comprobarNumero(numero)) {
+    break;
+  }
+}
